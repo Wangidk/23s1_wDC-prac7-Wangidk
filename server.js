5a405df5984f4e73a8694815991c61b0d9f008e8
dongbu <1129612616@qq.com>
2023-05-19 16:40:39 +0800

    11

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+let previousMessage = '';
 
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,6 +19,18 @@ app.get('/brew', (req, res) => {
       res.status(400).send();
     }
   });
+
+app.post('/pass-it-on', (req, res) => {
+  const message = req.body.message;
+
+  if (!message || message.trim() === '') {
+    res.status(400).send();
+  } else {
+    const responseMessage = previousMessage === '' ? message : previousMessage;
+    previousMessage = message;
+    res.send(responseMessage);
+  }
+});
 // 启动服务器
 const port = 3000;
 app.listen(port, () => {
