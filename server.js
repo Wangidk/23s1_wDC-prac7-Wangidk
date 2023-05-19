fe85d6d4fa0c6211788e8d0cd6ea02a7fe3698f7
dongbu <1129612616@qq.com>
2023-05-19 17:15:52 +0800

    11

--- server.js
+++ server.js
@@ -1,32 +1,11 @@
-const express = require('express');
-const session = require('express-session');
+app.get('/brew', (req, res) => {
+  const drink = req.query.drink;
 
-const app = express();
-const port = 3000;
-
-// Configure session middleware
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true
-}));
-
-// POST route for /tcaccept
-app.post('/tcaccept', (req, res) => {
-  req.session.accepted = true; // Set session flag to true
-  res.send('It works');
-});
-
-// GET route for /users/accepted
-app.get('/users/accepted', (req, res) => {
-  if (req.session.accepted) {
-    res.send('It works');
+  if (drink === 'tea') {
+    res.send('A delicious cup of tea!');
+  } else if (drink === 'coffee') {
+    res.sendStatus(418);
   } else {
-    res.sendStatus(403);
+    res.sendStatus(400);
   }
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
