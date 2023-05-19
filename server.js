9fa7e708030b93a8062de954704a813acdfa5da9
dongbu <1129612616@qq.com>
2023-05-19 17:34:20 +0800

    11

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+const express = require('express');
+const app = express();
+
 app.get('/brew', (req, res) => {
   const drink = req.query.drink;
 
   if (drink === 'tea') {
     res.send('A delicious cup of tea!');
   } else if (drink === 'coffee') {
-    res.sendStatus(418);
+    res.status(418).send();
   } else {
-    res.sendStatus(400);
+    res.status(400).send();
   }
+});
+
+// 启动服务器监听在指定的端口上
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
