8213c88570df59efb1a146b5cc5b3d9ecbbc7285
dongbu <1129612616@qq.com>
2023-05-19 20:24:29 +0800

    11

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+// 将public文件夹作为静态文件目录
+app.use(express.static('public'));
+// 设置 body-parser 中间件来解析请求体
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get('/brew', (req, res) => {
   const drink = req.query.drink;
 
@@ -11,6 +16,17 @@ app.get('/brew', (req, res) => {
   } else {
     res.status(400).send();
   }
+});
+// 添加这段代码作为路由处理函数
+app.post('/pass-it-on', (req, res) => {
+  const message = req.body.message;
+
+  // 处理消息，将后缀添加到每一行后面
+  const lines = message.split('\n');
+  const modifiedLines = lines.map(line => line + req.body.suffix);
+  const responseText = modifiedLines.join('\n');
+
+  res.send(responseText);
 });
 
 // 启动服务器监听在指定的端口上
